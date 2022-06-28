54a8c5af29264fdfde4090fc988dae6a2db212d4
wangxiaorui19 <wangxiaorui19@jd.com>
2022-06-28 19:53:50 +0800

    comomit

--- WCache/WDiskCache.hpp
+++ WCache/WDiskCache.hpp
@@ -18,29 +18,12 @@ namespace WDCache {
 class WDiskCache {
     
 public:
-    WDiskCache(const char *rootPath, const char *tableName): _rootPath(rootPath), _tableName(tableName) {
-        if (!_db) {
-            int result = sqlite3_open(_rootPath, &_db);
-            if (result == SQLITE_OK) {
-                std::cout << "_db创建成功" << std::endl;
-                const char *sql = "CREATE TABLE IF NOT EXISTS t_WDiskCache (id integer PRIMARY KEY AUTOINCREMENT, value blob NOT NULL, key text NOT NULL, size integer NOT NULL, modification_time integer, last_access_time integer);";
-                char * errorMsg;
-                sqlite3_exec(_db, sql, NULL, NULL, &errorMsg);
-                if (errorMsg) {
-                    std::cout << "表创建失败" << std::endl;
-                } else {
-                    std::cout << "表创建成功" << std::endl;
-                }
-            } else {
-                std::cout << "_db创建失败" << std::endl;
-            }
-        }
-    }
+    WDiskCache(const char *rootPath, const char *tableName);
     
 private:
     const char * _rootPath;
     const char * _tableName;
-    sqlite3 *_db;
+    sqlite3 *_db = NULL;
 };
 
 };
